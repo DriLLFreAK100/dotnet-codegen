4cbca13b05f566d385c778eead5d9feb258ed03e
drillfreak100 <blusky_8893@hotmail.com>
2022-06-01 23:32:06 +0800

    feat: include generation flow outline

--- CodeGenerator/TypeScript.cs
+++ CodeGenerator/TypeScript.cs
@@ -1,4 +1,6 @@
+using CodeGenerator.Attributes;
 using CodeGenerator.Models;
+using CodeGenerator.Utils;
 
 namespace CodeGenerator
 {
@@ -8,6 +10,22 @@ namespace CodeGenerator
 
         public override List<Output> GenerateMetadata()
         {
+            // 1 - Get all annotated
+            var types = GetAnnotatedTypes<GenerateTsAttribute>();
+            //var store = types.ToDictionary(x => x, _ => new Output());
+
+            // 2 - Get recursively all subtypes
+            types = types
+                .SelectMany(type => type.GetTargetTypes())
+                .Distinct()
+                .ToList();
+
+            // 3 - Populate pre-meta info
+
+
+            // 4 - Generate metadata
+
+
             return new();
         }
     }
